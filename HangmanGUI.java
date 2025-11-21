dc4acdc5ca41d1eb87c77d473efdc2764c73ab4f
iansmith <iasmith@gmail.com>
2025-11-21 15:29:54 -0600

    Commit 4

--- HangmanGUI.java
+++ HangmanGUI.java
@@ -30,43 +30,45 @@ public class HangmanGUI extends JFrame {
     private JButton tryAgain;
     //another j panel
     private JPanel innerBox;
-    //West J Panel
+    //West J Panel for the left hand side of the
     private JPanel westBox;
     //display score box2
     private JLabel displayScore2;
-    //private center box
+    //Center box containing the drawings
     private HangmanCanvas centerBox;
-    //InnerCenterBox
+    //Inner JPanel object container for the textfield
     private JPanel innerCenterBox;
+    //dimension object to manipulate the size of the textfield
     private Dimension inputSize;
+    //Panel object for the bottom of the GUI
     private JPanel southBox;
     //Button variables (**Will change in favor of loop)
-    private JButton letterA;
-    private JButton letterB;
-    private JButton letterC;
-    private JButton letterD;
-    private JButton letterE;
-    private JButton letterF;
-    private JButton letterG;
-    private JButton letterH;
-    private JButton letterI;
-    private JButton letterJ;
-    private JButton letterK;
-    private JButton letterL;
-    private JButton letterM;
-    private JButton letterN;
-    private JButton letterO;
-    private JButton letterP;
-    private JButton letterQ;
-    private JButton letterR;
-    private JButton letterS;
-    private JButton letterT;
-    private JButton letterU;
-    private JButton letterV;
-    private JButton letterW;
-    private JButton letterX;
-    private JButton letterY;
-    private JButton letterZ;
+//    private JButton letterA;
+//    private JButton letterB;
+//    private JButton letterC;
+//    private JButton letterD;
+//    private JButton letterE;
+//    private JButton letterF;
+//    private JButton letterG;
+//    private JButton letterH;
+//    private JButton letterI;
+//    private JButton letterJ;
+//    private JButton letterK;
+//    private JButton letterL;
+//    private JButton letterM;
+//    private JButton letterN;
+//    private JButton letterO;
+//    private JButton letterP;
+//    private JButton letterQ;
+//    private JButton letterR;
+//    private JButton letterS;
+//    private JButton letterT;
+//    private JButton letterU;
+//    private JButton letterV;
+//    private JButton letterW;
+//    private JButton letterX;
+//    private JButton letterY;
+//    private JButton letterZ;
     private int count;
 
 
@@ -81,27 +83,32 @@ public class HangmanGUI extends JFrame {
         setLayout(new BorderLayout());
         //getContentPane().setLayout(new FlowLayout());
 
-        //create new object to display words\
+        //creates an array list containing the word to be guessed
         List<String> word = Arrays.asList("F","L","O","U","R");
+        //stores the length of the word
         int n = word.size();
 
-        String[] dashes = new String[n];
-        Arrays.fill(dashes, "_");
+//        String[] dashes = new String[n];
+//        Arrays.fill(dashes, "_");
 
+        //creates new JPanel object for where the word will be displayed
         textSlots = new JPanel();
-        //creates grid layout for a five letter word
+        //creates flowlayout layout for a five letter word
         textSlots.setLayout(new FlowLayout());
 
         //loop adds the dashes to where words should be
         for (int i = 0; i < n; i++) {
-            //adds dashes for where words will be displayed in the center of screen
+            //adds JLabel dashes for where words will be displayed in the center of screen
             textSlots.add(new JLabel("_", SwingConstants.CENTER));
             }
         //adds them to screen and centers them north at the top of the screen
         add(textSlots, BorderLayout.NORTH);
 
+        //Creates a JPanel object container for the bottom of the GUI
         southBox = new JPanel();
+        //setLayout is set to grid layout for buttons
         southBox.setLayout(new GridLayout(2, 13));
+        //adds the south box to the gui
         add(southBox, BorderLayout.SOUTH);
 
 //
@@ -321,25 +328,33 @@ public class HangmanGUI extends JFrame {
 //        southBox.add(letterZ, BorderLayout.SOUTH);
 
         //Button creation
+        //String array containing the alphabet
         String[] alphabet = {"A","B","C","D","E","F","G","H","I","J","K","L","M",
                 "N","O","P","Q","R","S","T","U","V","W","X","Y","Z"};
-        //new button object created
+        //For each creates new button objects for letters
             for (String c : alphabet) {
                 JButton b = new JButton(c);
+                //Action lister added to buttons to take input
                 b.addActionListener(e -> {
+                    //letters are set
                     textInput.setText(c);
                     // If the word contains the current alphabet letter
                     if (word.contains(c)) {
-                        for (int i = 0; i < word.size(); i++) {
+                        //loop through the word and compares the element at the index
+                        for (int i = 0; i < n; i++) {
                             if (c.equalsIgnoreCase(word.get(i))) {
+                                //stores the dashes in the temp variable
                                 JLabel temp = (JLabel) textSlots.getComponent(i);
+                                //draws the correct letter guessed at the appropriate index
                                 temp.setText(word.get(i));
                             }
                         }
                     }
                     // Else
                     else {
+                        //count increments if guessed wrong
                         count++;
+                        //displays the number of wrong guesses
                         displayScore2.setText(String.valueOf(count));
                     }
                     // Draw hangman + increase count
@@ -379,27 +394,27 @@ public class HangmanGUI extends JFrame {
         add(centerBox, BorderLayout.CENTER);
 
         //Text Field for input
+        //creates new JPanel container object
         innerCenterBox = new JPanel();
+        //adds the new container to the centerBox container
         centerBox.add(innerCenterBox);
+        //creates a text field object for taking input
         textInput = new JTextField("Enter a letter: ");
-//        textInput.setBounds(500, 500, 100, 20);
+        //creates a dimension object to set the width and height of the text field box
         inputSize = new Dimension(100, 20);
+        //methods sets the size of the text field
         textInput.setPreferredSize(inputSize);
+        //adds the textfield object to the container
         innerCenterBox.add(textInput, BorderLayout.SOUTH);
 
+        //TODO: Create method that checks if the game has been won.
 
-
-//
-
-        //tryAgain.setPreferredSize(buttonSize);
-//        tryAgain.setBounds(50, 50, 20, 50); // x=50, y=50, width=100, height=50
-//        add(tryAgain);
-//        tryAgain.setLayout(new GridLayout(2, 5));
-//        add(tryAgain);
+        //TODO: Create method to check if counter has reached 6, and the player has lost the game.
 
         setVisible(true);
     }
 
+    //Main method to run the game
     public static void main(String[] args) {
         HangmanGUI hg = new HangmanGUI();
         // user selects letter
